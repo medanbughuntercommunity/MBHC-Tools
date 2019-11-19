ede63a6134a03d185ea8d88d85b46463537a8242
denypradana <pradana.deny@gmail.com>
2019-11-19 23:06:41 +0700

    add portscanner

---
+++ portscanner/README.md
@@ -0,0 +1,10 @@
+# Port Scanner
+Simple Open Port Scanner by Deny Pradana
+
+Still far from complete but worth to try
+
+* **Install all requirements library first**
+  * sudo pip3 install -r requirements.txt
+
+* **Simple Usage**
+  * python3 scanport.py target_host
---
+++ portscanner/scanport.py
@@ -0,0 +1,79 @@
+import argparse
+import socket
+from colorama import init, Fore
+
+from threading import Thread, Lock
+from queue import Queue
+
+init()
+RED = Fore.RED
+GREEN = Fore.GREEN
+BLUE = Fore.BLUE
+RESET = Fore.RESET
+GRAY = Fore.LIGHTBLACK_EX
+
+N_THREADS = 200
+
+q = Queue()
+print_lock = Lock()
+
+def port_scan(port):
+    try:
+        s = socket.socket()
+        s.connect((host, port))
+    except:
+        with print_lock:
+            print(f"{GRAY}[-] {host:15}:{port:5} is closed {RESET}", end='\r')
+    else:
+        with print_lock:
+            print(f"{GREEN}[+] {host:15}:{port:5} is open {RESET}")
+    finally:
+        s.close()
+
+
+def scan_thread():
+    global q
+    while True:
+        worker = q.get()
+        port_scan(worker)
+        q.task_done()
+
+def main(host, ports):
+    print(f"{RED}##################################")
+    print(f"##  __  __ ____  _    _  _____  ##")
+    print(f"## |  \/  |  _ \| |  | |/ ____| ##")
+    print(f"## | \  / | |_) | |__| | |      ##")
+    print(f"## | |\/| |  _ <|  __  | |      ##")
+    print(f"## | |  | | |_) | |  | | |____  ##")
+    print(f"## |_|  |_|____/|_|  |_|\_____| ##")
+    print(f"##                              ##")
+    print(f"## ScanPort ver 0.1 beta        ##")
+    print(f"## Open Port Scanner            ##")
+    print(f"##################################{RESET}")
+    print("")
+    print(f"{BLUE}RESULT : {RESET}")
+    global q
+    for t in range(N_THREADS):
+        t = Thread(target=scan_thread)
+        t.daemon = True
+        t.start()
+
+    for worker in ports:
+        q.put(worker)
+    
+    q.join()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="MBHC open port scanner")
+    parser.add_argument("host", help="Host to scan.")
+    parser.add_argument("--ports", "-p", dest="port_range", default="1-65535", help="Port range to scan, default is 1-65535 (all ports)")
+    args = parser.parse_args()
+    host, port_range = args.host, args.port_range
+
+    start_port, end_port = port_range.split("-")
+    start_port, end_port = int(start_port), int(end_port)
+
+    ports = [ p for p in range(start_port, end_port)]
+
+    main(host, ports)
