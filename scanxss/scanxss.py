6290296cc4de9d60b464cdf3eef8aad27d72429c
denypradana <pradana.deny@gmail.com>
2019-11-19 15:17:37 +0700

    add more details and some error handler

--- scanxss/scanxss.py
+++ scanxss/scanxss.py
@@ -1,16 +1,27 @@
-import sys
-import requests
-from pprint import pprint
-from bs4 import BeautifulSoup as bs
-from urllib.parse import urljoin
-from colorama import init, Fore
+#!/usr/bin/env python3
+from core.colors import end, red, white, green, bad, good, info, run
 
-init()
-RED = Fore.RED
-GREEN = Fore.GREEN
-BLUE = Fore.BLUE
-RESET = Fore.RESET
-GRAY = Fore.LIGHTBLACK_EX
+try:
+    import requests
+except ImportError:
+    import os
+    print('%s requests isn\'t installed, installing now...' % info)
+    os.system('pip3 install requests')
+    print('%s requests has been installed, restart ScanXSS.' % info)
+    quit()
+try:
+    from bs4 import BeautifulSoup as bs
+except ImportError:
+    import os
+    print('%s bs4 isn\'t installed, installing now...' % info)
+    os.system('pip3 install bs4')
+    print('%s bs4 has been installed, restart ScanXSS.' % info)
+    quit()
+
+import sys, getopt
+from pprint import pprint
+from urllib.parse import urljoin
+from urllib.parse import urlparse
 
 def get_all_forms(url):
     soup = bs(requests.get(url).content, "html.parser")
@@ -51,7 +62,7 @@ def submit_form(form_details, url, value):
 
 def scan_xss(url):
     forms = get_all_forms(url)
-    print(f"{BLUE}[+] Detected {len(forms)} forms on {url}. {RESET}")
+    print(f"%s Detected {len(forms)} forms on {url}." % run)
     inp = open("payloads.txt","r")
     for line in inp:
         js_script = line.strip()
@@ -61,24 +72,46 @@ def scan_xss(url):
             form_details = get_form_details(form)
             content = submit_form(form_details, url, js_script).content.decode()
             if js_script in content:
-                print(f"{GREEN}[+] XSS Detected on {url} {RESET}")
-                print(f"[*] Form details:")
+                print(f"%s XSS Detected on {url}" % good)
+                print(f"%s Form details:" % info)
                 pprint(form_details)
                 is_vulnerable = True
                 
         return is_vulnerable
+from urllib.parse import urlparse
+
+def is_url(url):
+  try:
+    result = urlparse(url)
+    return all([result.scheme, result.netloc])
+  except ValueError:
+    return False
+
+def main(argv):
+    if not is_url(argv):
+        print(f"%s Not a valid url !" % bad)
+        print(f"%s Use standard url format. Example : http://{argv}.com, http://web.{argv}.com !" % bad)
+        quit()
+
+    print('%s##################################' % red)
+    print('##  __  __ ____  _    _  _____  ##')
+    print('## |  \/  |  _ \| |  | |/ ____| ##')
+    print('## | \  / | |_) | |__| | |      ##')
+    print('## | |\/| |  _ <|  __  | |      ##')
+    print('## | |  | | |_) | |  | | |____  ##')
+    print('## |_|  |_|____/|_|  |_|\_____| ##')
+    print('##                              ##')
+    print('## ScanXSS ver 0.2 beta         ##')
+    print('## XSS Vulnerability Scanner    ##')
+    print('##################################')
+    print('%s' % end)
+    print(scan_xss(argv))
 
 if __name__ == "__main__":
-
-    url = sys.argv[1]
-    print(f"{RED}##################################")
-    print(f"##  __  __ ____  _    _  _____  ##")
-    print(f"## |  \/  |  _ \| |  | |/ ____| ##")
-    print(f"## | \  / | |_) | |__| | |      ##")
-    print(f"## | |\/| |  _ <|  __  | |      ##")
-    print(f"## | |  | | |_) | |  | | |____  ##")
-    print(f"## |_|  |_|____/|_|  |_|\_____| ##")
-    print(f"##                              ##")
-    print(f"## XSS Scanner ver 0.1 beta     ##")
-    print(f"################################## {RESET}")
-    print(scan_xss(url))
+    appname = sys.argv[0]
+    try:
+        main(sys.argv[1])
+    except IndexError:
+        print(f"%s Input Error !" % bad)
+        print(f"%s Usage : python3 {appname} <target_url>" % bad)
+        quit()
